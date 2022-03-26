076511559986fe95abebef659710e961e1c7a060
Nick Bukovskiy <sonterixo@gmail.com>
2022-03-26 04:23:21 +0200

    Base click functionality

---
+++ src/helpers/index.js
@@ -0,0 +1,2 @@
+// Get random number (min and max included)
+export const getRandomNubmer = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
---
+++ src/index.js
@@ -0,0 +1,120 @@
+import { Application, Container, Graphics } from 'pixi.js'
+
+import { getRandomNubmer } from './helpers'
+import './styles/styles.css'
+
+class Gameplay {
+  app
+
+  scene
+
+  circleInterval
+
+  lives = 3
+
+  score = 0
+
+  items = []
+
+  constructor(scene, params) {
+    this.app = new Application(params)
+    this.scene = scene
+
+    this.scene.appendChild(this.app.view)
+  }
+
+  start() {
+    // Creating container for the circles
+    const circlesContainer = this.createContainer()
+    this.app.stage.addChild(circlesContainer)
+
+    // Creating circles each 500ms
+    this.circleInterval = setInterval(() => {
+      const circle = this.createCircle()
+      // Push each new circle to the state
+      this.items.push(circle)
+      // Append circle item itself
+      circlesContainer.addChild(circle.graphics)
+    }, 500)
+
+    this.app.ticker.add(delta => {
+      this.items.forEach(({ id, active, graphics, weight }) => {
+        if (active) {
+          graphics.y += weight + delta
+        } else {
+          this.removeCircle(id)
+        }
+      })
+    })
+  }
+
+  end() {}
+
+  createContainer() {
+    const container = new Container()
+    this.app.stage.addChild(container)
+
+    return container
+  }
+
+  createCircle() {
+    const id = getRandomNubmer(0, 9999999)
+    const active = true
+    // Set fall speed
+    const weight = getRandomNubmer(0, 5)
+    // Get random circle radius
+    const radius = getRandomNubmer(20, 70)
+    // Get random horizontal position
+    const xPos = getRandomNubmer(radius / 2, this.scene.clientWidth - radius / 2)
+    // Get vertical position above viewport
+    const yPos = -radius
+    // Create cricle element
+    const graphics = new Graphics()
+    graphics.beginFill(0xffffff)
+    graphics.drawCircle(0, 0, radius)
+    graphics.position.set(xPos, yPos)
+    graphics.interactive = true
+    graphics.buttonMode = true
+    graphics.endFill()
+    // Create circle item
+    const circle = { id, active, weight, graphics }
+    // Handle remove event on touch
+    graphics.on('mousedown', () => this.disableCircle(id)).on('touchstart', () => this.disableCircle(id))
+
+    return circle
+  }
+
+  disableCircle(id) {
+    // Search for item by id
+    const { graphics } = this.items.find(({ id: itemId }) => itemId === id) || {}
+
+    if (graphics) {
+      // Change graphics status
+      this.items = this.items.map(item => (item.id === id ? { ...item, active: false } : item))
+      graphics.interactive = false
+    }
+  }
+
+  removeCircle(id) {
+    const { graphics } = this.items.find(({ id: itemId }) => itemId === id) || {}
+
+    if (graphics && (graphics.scale.x < 0 || graphics.scale.y < 0)) {
+      // Remove graphics
+      this.items = this.items.filter(item => item.id !== id)
+      graphics.destroy()
+    } else {
+      // Scale down the graphics
+      graphics.scale.set(graphics.scale.x - 0.1, graphics.scale.y - 0.1)
+    }
+  }
+}
+
+// Get scene and params
+const scene = document.querySelector('#scene')
+const params = {
+  width: scene?.clientWidth,
+  height: scene?.clientHeight
+}
+// Create the app instance
+const app = new Gameplay(scene, params)
+app.start()
